c4151cfd71b84af93763ae8e594990a6ab467418
german vertel <gvertel88@gmail.com>
2024-08-20 11:17:48 -0500

    Initial Commit

---
+++ ToDo App/Model/Task.swift
@@ -0,0 +1,39 @@
+//
+//  Task.swift
+//  ToDo App
+//
+//  Created by German David Vertel Narvaez on 19/08/24.
+//
+
+import Foundation
+
+enum Priority: Int {
+    case low = 1
+    case normal = 2
+    case hight = 3
+    
+    func toString() -> String {
+        switch self {
+        case .low:
+            return "Baja"
+        case .normal:
+            return "Normal"
+        case .hight:
+            return "Alta "
+        }
+    }
+}
+
+@Observable class Task: Identifiable{
+    var id : UUID
+    var title: String
+    var priority: Priority
+    var isCompleted : Bool
+    
+    init(id: UUID = UUID(), title: String = "", priority: Priority = .normal, isCompleted: Bool = false) {
+        self.id = id
+        self.title = title
+        self.priority = priority
+        self.isCompleted = isCompleted
+    }
+}
