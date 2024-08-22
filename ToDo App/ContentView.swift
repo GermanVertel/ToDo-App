25d58223c1a4281a6c6f0187eba8cc25143de94a
german vertel <gvertel88@gmail.com>
2024-08-22 12:02:47 -0500

    se estrictura lo que va a ser la pantalla inicial de la lista de tareas

--- ToDo App/ContentView.swift
+++ ToDo App/ContentView.swift
@@ -9,53 +9,71 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    
+    //MARK: LISTA DE TAREAS
+    @State var tasks: [Task] = []
+    
+    //MARK: CREAR NUEVA TAREA
+    
+    @State private var newTaskTitle: String = ""
+    @State private var newTaskPriority: Priority = .normal
+    
+    @State private var showNewTask = false
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+        ZStack{
+            VStack{
+                HStack {
+                    Text("Lista de tareas")
+                        .font(.system(size: 30, weight: .bold, design: .rounded))
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        self.showNewTask = true
+                    }, label: {
+                        Image(systemName: "plus.circle")
+                            .font(.title)
+                            .foregroundStyle(.teal)
+                    })
+                }.padding()
+                
+                
+                
+                //Codigo para la lista de tareas
+                
+                List{
+                    ForEach(self.tasks) { task in
+                        //TODO: Representar la tarea en una fila
+                        
                     }
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
+                .listStyle(.plain) //estilo de la lista
+                .rotation3DEffect(
+                    Angle(degrees: self.showNewTask ? 8 : 0), axis: (x: 1, y: 0, z: 0))
+                .offset(y: self.showNewTask ? -40 : 0)
+                .animation(.easeInOut, value: self.showNewTask)
+                //CUANDO APAREZCA
+                .onAppear{
+                    UITableView.appearance().separatorColor = .clear
                 }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
+                
+                if self.tasks.count == 0 {
+                    //TODO: CREAR VISTA VACIA
+                }
+                
+                if self.showNewTask {
+                    //TODO: CREAR EL FORMULARIO PARA UNA NUEVA TAREA
                 }
             }
-        } detail: {
-            Text("Select an item")
         }
+        
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+    
 }
+    
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+       
 }
